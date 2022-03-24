4ddbfa5f7b24e02375d1b8820d2f552fdde2b022
Bertrand Dubaut <bdubaut@hey.com>
2022-03-24 14:24:58 +0100

    feat(hello): add a good bye message ability

--- lib/git_bisect_app/hello/message.ex
+++ lib/git_bisect_app/hello/message.ex
@@ -2,9 +2,14 @@ defmodule GitBisectApp.Hello.Message do
   @moduledoc false
 
   @message "Hello World!"
+  @good_bye "Sayonara"
 
   @spec hello :: binary()
   def hello() do
     @message
+  end
+
+  def good_bye() do
+    @good_bye
   end
 end
